7e7884d0bc925bf3dc44cfd1d077859efe2cf6b7
houssammaassam55-debug <houssammaassam55@gmail.com>
2025-08-29 02:23:22 +0100

    Create App.js

---
+++ Src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>السلام عليكم 👋</h1>
+      <p>تطبيق الريأكت شغال بنجاح!</p>
+    </div>
+  );
+}
+
+export default App;
