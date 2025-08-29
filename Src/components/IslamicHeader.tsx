03fdf675d901564ba07e9a73da21f878327e7d9a
houssammaassam55-debug <houssammaassam55@gmail.com>
2025-08-29 01:58:04 +0100

    Create IslamicHeader.tsx

---
+++ Src/components/IslamicHeader.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+export const IslamicHeader: React.FC = () => (
+  <header style={{padding: 16, textAlign: 'center', background: '#16a34a', color: 'white', fontWeight: 700}}>
+    تطبيق إسلامي
+  </header>
+)
+export default IslamicHeader
